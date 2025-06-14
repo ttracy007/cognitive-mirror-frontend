ecded7187d45cdc0dd8029e7b93b03efe7f59327
ttracy007 <freeholdprods@gmail.com>
2025-06-14 16:08:19 -0600

    Old Order Fuctions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import SummaryViewer from './SummaryViewer'; 
+import SummaryViewer from './SummaryViewer';  
 import { supabase } from './supabaseClient';
 import './App.css';
 import LandingPage from './LandingPage';
