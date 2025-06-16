2407314cdf88b80495852e3acd9525b08a3b8651
ttracy007 <freeholdprods@gmail.com>
2025-06-16 00:41:13 -0600

    Insert console.log "sending journal entry" after full fetch function

--- src/App.js
+++ src/App.js
@@ -97,7 +97,6 @@ const App = () => {
       
       const res = await fetch(process.env.REACT_APP_BACKEND_URL + '/journal-entry', {
         method: 'POST',
-            console.log("sending journal entry:");
         body: JSON.stringify({
           entry_text: entry_text,
           tone_mode: forcedTone,
@@ -105,6 +104,7 @@ const App = () => {
           user_id: userId
         }),
       });
+                  console.log("sending journal entry:");
 
       if (!res.ok) {
         const errorResponse = await res.json();
