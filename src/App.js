af3bfa4b7be0cb9e0fd453eea24cdc106809dc80
ttracy007 <freeholdprods@gmail.com>
2025-06-19 16:22:55 -0600

    placed ' backtick inside )

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ const App = () => {
       .then(res => res.txt())
       .then(text => 
          console.log("🛠️ App.js version:", text);
-         console.log(`🧱 Frontend build version: ${text})`;
+         console.log(`🧱 Frontend build version: ${text}`);
       });
   }, []);
   
