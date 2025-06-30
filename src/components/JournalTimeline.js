e31ad02affca12a298e19a36f9d19eef92e17641
ttracy007 <freeholdprods@gmail.com>
2025-06-29 22:04:06 -0600

    Update JournalTimeline.js

--- src/components/JournalTimeline.js
+++ src/components/JournalTimeline.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../supabaseClient'; 
 import dayjs from 'dayjs';
 import groupBy from 'lodash.groupby';
 
