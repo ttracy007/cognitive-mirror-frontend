2d820b135eafc436994a86261be9a7e9c4f8320f
ttracy007 <freeholdprods@gmail.com>
2025-07-06 20:35:33 -0600

    Insert supabase into import

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { supabase } from './supabaseClient';
 
 export default function LandingPage({ onLogin }) {
   const [username, setUsername] = useState('');
