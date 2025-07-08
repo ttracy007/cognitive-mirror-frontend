f2204fc3a04375c2004a7d061a3841f0209a5bb7
ttracy007 <freeholdprods@gmail.com>
2025-07-08 15:33:32 -0600

    related Aliases to relatedTopics

--- src/components/JournalTimeline.js
+++ src/components/JournalTimeline.js
@@ -123,7 +123,7 @@ export default function JournalTimeline({userId, refreshTrigger }) {
         
           return {
             ...entry,
-            aliases: relatedAliases,
+            aliases: relatedTopics,
           };
         });
 
