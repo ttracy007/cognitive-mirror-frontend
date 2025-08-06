2b76534f59ff7240f322d9c0b104e2e4a4304bad
ttracy007 <freeholdprods@gmail.com>
2025-08-06 17:59:46 -0600

    Remove duplicate useState entry

--- src/App.js
+++ src/App.js
@@ -21,10 +21,9 @@ const App = () => {
   const [parsedTags, setParsedTags] = useState([]);
   const [severityLevel, setSeverityLevel] = useState('');
   const [isProcessing, setIsProcessing] = useState(false);
-  const [username, setUsername] = useState('');
   const [recognition, setRecognition] = useState(null);
   const [isListening, setIsListening] = useState(false);
-  const prompts = ["What’s weighing you down?"];
+  const prompts = ["What’s shakkng sugar?"];
   const [showGroupedView, setShowGroupedView] = useState(false);
   const [placeholderPrompt, setPlaceholderPrompt] = useState(() =>
     prompts[Math.floor(Math.random() * prompts.length)] 
