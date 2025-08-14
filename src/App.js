0f186d1caca7e952957a501cd050b23c52332c9a
ttracy007 <freeholdprods@gmail.com>
2025-08-14 16:53:01 -0600

    change paddingBottom journalTimeline from 220px to 1rem

--- src/App.js
+++ src/App.js
@@ -595,7 +595,7 @@ return (
         {/* END fixed input container */}
 
         {/* Timeline (outside the fixed container) */}
-        <div style={{ flex: 1, overflowY: 'auto', paddingBottom: '240px' }}>
+        <div style={{ flex: 1, overflowY: 'auto', paddingBottom: 1rem' }}>
           <JournalTimeline userId={session?.user?.id} refreshTrigger={refreshTrigger} styleVariant={styleVariant} />
         </div>
 
