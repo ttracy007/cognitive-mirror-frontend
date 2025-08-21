b09569108a845e1a26e6dfc0e48ceb0bddc45666
ttracy007 <freeholdprods@gmail.com>
2025-08-21 02:14:13 -0600

    block out conflicting keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qktrfgxocmemivtwcink.supabase.co' 
-const supabaseKey = 'sb_publishable_7NICQC4zdbCM5Pk9rKfPBg_YypmqNRD'
+// const supabaseUrl = 'https://qktrfgxocmemivtwcink.supabase.co' 
+// const supabaseKey = 'sb_publishable_7NICQC4zdbCM5Pk9rKfPBg_YypmqNRD'
 
 // export const supabase = createClient(supabaseUrl, supabaseKey)
 
