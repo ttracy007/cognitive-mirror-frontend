786817ba2e1c9a4b82919fbcad23f8a8d8f497f6
ttracy007 <freeholdprods@gmail.com>
2025-08-21 12:59:12 -0600

    Test: change supabaseClient js to match dev dynamic calls

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xiwwboyozcbflldgnsmz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhpd3dib3lvemNiZmxsZGduc216Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxNDM1MTYsImV4cCI6MjA2NDcxOTUxNn0.6UlKagvbR1tBMBp4bY_3qytcwFxDFcS7HcibHMd5K-s'
+// const supabaseUrl = 'https://qktrfgxocmemivtwcink.supabase.co' 
+// const supabaseKey = 'sb_publishable_7NICQC4zdbCM5Pk9rKfPBg_YypmqNRD'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+// export const supabase = createClient(supabaseUrl, supabaseKey)
 
+export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY)
