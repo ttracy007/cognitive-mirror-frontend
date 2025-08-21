ba3685b1ab41918852f75315385aaced01fc0e38
ttracy007 <freeholdprods@gmail.com>
2025-08-20 23:09:23 -0600

    swap in -- new supabase dev API generated published key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qktrfgxocmemivtwcink.supabase.co' 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrdHJmZ3hvY21lbWl2dHdjaW5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2NTk4NjcsImV4cCI6MjA3MTIzNTg2N30.xkIVAn2IrbOHHGAeOnk8VWURPd8Xau8OfNy930cIMNI'
+const supabaseKey = 'sb_publishable_7NICQC4zdbCM5Pk9rKfPBg_YypmqNRD
+Service Role Key
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrdHJmZ3hvY21lbWl2dHdjaW5rIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTY1OTg2NywiZXhwIjoyMDcxMjM1ODY3fQ.V0M-WgHSoR3EPcMt9yvgh7zNPt11b5lnl177ntlEk-w'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
