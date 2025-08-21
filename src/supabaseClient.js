05cb5afcf1fe74d0c3f69f298a3d24665b089a9a
ttracy007 <freeholdprods@gmail.com>
2025-08-20 21:20:49 -0600

    update supabase client info for dev only stack

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xiwwboyozcbflldgnsmz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhpd3dib3lvemNiZmxsZGduc216Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxNDM1MTYsImV4cCI6MjA2NDcxOTUxNn0.6UlKagvbR1tBMBp4bY_3qytcwFxDFcS7HcibHMd5K-s'
+const supabaseUrl = 'https://qktrfgxocmemivtwcink.supabase.co' 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrdHJmZ3hvY21lbWl2dHdjaW5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2NTk4NjcsImV4cCI6MjA3MTIzNTg2N30.xkIVAn2IrbOHHGAeOnk8VWURPd8Xau8OfNy930cIMNI
+'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
