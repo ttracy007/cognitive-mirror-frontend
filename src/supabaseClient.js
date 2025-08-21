7fcf35375e99e3cba683d1ea1cbde8daa4f41099
ttracy007 <freeholdprods@gmail.com>
2025-08-20 23:11:04 -0600

    fix: enter correct anon key new Dev API

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qktrfgxocmemivtwcink.supabase.co' 
-const supabaseKey = 'sb_publishable_7NICQC4zdbCM5Pk9rKfPBg_YypmqNRD
-Service Role Key
-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrdHJmZ3hvY21lbWl2dHdjaW5rIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTY1OTg2NywiZXhwIjoyMDcxMjM1ODY3fQ.V0M-WgHSoR3EPcMt9yvgh7zNPt11b5lnl177ntlEk-w'
+const supabaseKey = 'sb_publishable_7NICQC4zdbCM5Pk9rKfPBg_YypmqNRD'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
