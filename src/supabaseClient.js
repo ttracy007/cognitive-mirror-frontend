b2726f068473482dee2fe55f9d2f9a0ddea6fc32
ttracy007 <freeholdprods@gmail.com>
2025-08-22 15:45:44 -0600

    fix: make supabase sessions sticky

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,11 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY,
-    {
-        auth: {
-            persistSession: true,
-            autoRefreshToken: true,
-            detectSessionInUrl: true,
-            storage: window.localStorage
-        }
-    })
+export const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      storageKey: 'cm-auth', // namespaced
+    },
+  }
+)
