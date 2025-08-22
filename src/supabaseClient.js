cd10bd006fc542b470310abba4ceabd134772b33
ttracy007 <freeholdprods@gmail.com>
2025-08-22 15:16:25 -0600

    remove commented out legacy block

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 
-// const supabaseUrl = 'https://qktrfgxocmemivtwcink.supabase.co' 
-// const supabaseKey = 'sb_publishable_7NICQC4zdbCM5Pk9rKfPBg_YypmqNRD'
-
-// export const supabase = createClient(supabaseUrl, supabaseKey)
-
 export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY)
 
